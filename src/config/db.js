53ee32782db15f7e576ae2a4eaebcef8c107d79a
Raj Singh <rajsinghrk7786@gmail.com>
2026-01-06 22:30:06 +0530

    Use MONGO_URI from environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://rajsinghrk7786_db_user:admin@cluster0.dg1fbjg.mongodb.net/?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log("MongoDB connected successfully");
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
